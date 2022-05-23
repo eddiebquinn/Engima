285d1d0e3107b1cdd3a6ca2bb94b1e8b4bb15238
eddiebquinn <github@pranonym.com>
2022-05-23 17:50:51 +0100

    feat: created code to account for ring setting

--- rotors.py
+++ rotors.py
@@ -12,11 +12,30 @@ class Rotor():
     def get_map(self, rotor_name: str, ring_setting: int):
         json_file_path = f"rotor_maps/{rotor_name}.json"
         with open(json_file_path, "r") as j:
-            content = json.loads(j.read())
+            map_ = json.loads(j.read())
 
-        # Write code that offsets map based on ring setting, here
+        if self.ring_setting != 0:
+            map_["map"] = self.offset_map(map_["map"])
 
-        return content
+        return map_
+
+    def offset_map(self, map_: dict):
+        keys = list(map_.keys())
+        old_vals = list(map_.values())
+        new_map = {}
+        for key in keys:
+            index = keys.index(key)
+            new_index = self.get_index(index)
+            new_map[key] = old_vals[new_index]
+        return new_map
+
+    def get_index(self, index: int):
+        max_val = (25 - self.ring_setting) + 1
+        if index >= max_val:
+            new_index = index - max_val
+        else:
+            new_index = index + self.ring_setting
+        return new_index
 
     def click(self):
         self.offset = 0 if self.offset == 25 else self.offset + 1
